fd4d489c0542ec2455895ecbad9f150cd79251d8
Ramkumar Velmurugan <vrk1990@outlook.com>
2020-03-21 15:58:54 -0400

    Paper db integrated

--- app/src/main/java/com/check/coupon/repository/CouponRepository.kt
+++ app/src/main/java/com/check/coupon/repository/CouponRepository.kt
@@ -1,15 +1,19 @@
 package com.check.coupon.repository
 
+import android.util.Log
 import com.check.coupon.di.component.DaggerNetworkComponent
 import com.check.coupon.model.Offer
 import com.check.coupon.service.CouponApi
+import com.check.coupon.util.Constants
+import io.paperdb.Paper
+import java.lang.Exception
 import javax.inject.Inject
 
 class CouponRepository private constructor() {
 
     companion object {
         var single =  CouponRepository()
-        lateinit var offerList:ArrayList<Offer>
+        var offerList:ArrayList<Offer> = ArrayList()
         fun getInstance(): CouponRepository {
             if (single == null)
                 single = CouponRepository()
@@ -26,10 +30,22 @@ class CouponRepository private constructor() {
     lateinit var api: CouponApi
 
     suspend fun initialize() {
-        offerList = api.getCoupon().offers as ArrayList<Offer>
+        try {
+            offerList = api.getCoupon().offers as ArrayList<Offer>
+            if(offerList.isNotEmpty())
+                Paper.book().write(Constants.OFFER_CACHE,offerList)
+        } catch (e:Exception) {
+            Log.d("Api Error","Error in remote source")
+        }
+
+
     }
 
     fun getOffers():ArrayList<Offer> {
+        // read data from the cache
+        if(offerList.isEmpty()) {
+            return Paper.book().read(Constants.OFFER_CACHE) as ArrayList<Offer>
+        }
         return offerList
     }
 
--- app/src/main/java/com/check/coupon/util/Constants.kt
+++ app/src/main/java/com/check/coupon/util/Constants.kt
@@ -3,5 +3,7 @@ package com.check.coupon.util
 class Constants {
     companion object {
         val BASE_URL = "https://www.spkdroid.com"
+
+        val OFFER_CACHE = "offer_db";
     }
 }
